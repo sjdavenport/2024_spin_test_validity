ac41bbb6220726460185cfc3ed73aeaecd945407
BrainStatsSam <12SDavenport@gmail.com>
2024-02-19 16:23:22 +0100

    backup

--- LoadOasisFiles/oasis_fpr_singlesubject.m
+++ LoadOasisFiles/oasis_fpr_singlesubject.m
@@ -1,6 +1,7 @@
-function oasis_fpr_singlesubject(FWHM, nsims, nperm)
+function fpr = oasis_fpr_singlesubject(FWHM, nsims, nperm)
 
-nsubj_oasis = 432;
+oasis_data_dir = '';
+oasis_subject_files = filesindir(oasis_data_dir);
 
 if ~exist('nsims', 'var')
     nsims = 1000;
@@ -9,31 +10,36 @@ if ~exist('nperm', 'var')
     nperm = 1000;
 end
 
+fpr = 0;
+
 for I = 1:nsims
-    subject_id = randsample(1:nsubj_oasis, 1);
+    id = randsample(1:length(oasis_subject_files), 1);
+    subject_file = [oasis_data_dir, oasis_subject_files(id)];
 
     % Load in the subject data
     clear srf
 
     % Load in the surface info for subject 1
-    fs1_left = '';
-    fs1_right = '';
-    fs1_sphere_left = '';
-    fs1_sphere_right = '';
-    srf = fs2surf(fs1_left, fs1_right);
-    srf_sphere = fs2surf(fs1_sphere_left, fs1_sphere_right);
+    srf_sphere_data = load(subject_file);
+    srf_sphere = srf_sphere_data.srf;
+    % fs1_left = '';
+    % fs1_right = '';
+    % fs1_sphere_left = '';
+    % fs1_sphere_right = '';
+    % srf = fs2surf(fs1_left, fs1_right);
+    % srf_sphere = fs2surf(fs1_sphere_left, fs1_sphere_right);
 
     % Add data and smooth
-    surface_data.lh = randn(srf.lh.nvertices, 1);
-    surface_data.rh = randn(srf.rh.nvertices, 1);
-    srf = smooth_surface(srf, surface_data, FWHM);
+    X = surf_noise( srf_sphere, FWHM, 'ones' );
+    Y = surf_noise( srf_sphere, FWHM, 'ones' );
 
-    % Spin the surface data for subject 1
-    [ left_rotations,  right_rotations] = spin_surface( srf, srf_sphere, nperm );
-
-    % Do spin test bit here!
-    
+    % Run the spin test
+    [thresh, rho_store] = spintest( X, Y, srf_sphere, nperm, 0 );
+    if rho_store(1) > thresh
+        fpr = fpr + 1;
+    end
 end
 
+fpr = fpr/nsim;
 
 end
