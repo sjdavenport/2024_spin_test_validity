b5afe38173a592b19c8d51f6bb4b084b023cc256
BrainStatsSam <12SDavenport@gmail.com>
2024-02-22 18:35:42 +0100

    backup

--- freesurfer_scripts/nosmoothiebert.sh
+++ freesurfer_scripts/nosmoothiebert.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-for n in 5 ; do
+for n in 3 5 ; do
 TARGET=fsaverage${n}
 mkdir -p  $/home/sdavenport/freesurfer_runs/mris_preproc
 LIST=""
 for sub in $(cat /home/sdavenport/freesurfer_runs/listofsubjects.csv) ; do  #listofsubjects.csv needs to contain the subject folder names i.e. OAS...
 LIST="${LIST} --s ${sub}"
 done
-for f in 0 5 10 15; do
 for meas in area thickness; do
 for h in lh rh ; do
       ${FREESURFER_HOME}/bin/mris_preproc ${LIST} \
@@ -15,13 +14,11 @@ for h in lh rh ; do
       --target ${TARGET} \
       --meas ${meas} \
       --surfreg sphere.reg \
-      --out /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic${n}_fwhm${f}.mgz \
-      --fwhm ${f} \
+      --out /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic${n}.mgz \
       --nocleanup ;
-      rm -f /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic${n}_fwhm${f}.mgz ;
-      pid=$(awk '/tmpdir is/ {print $NF}' /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic${n}_fwhm${f}.mris_preproc.log | awk -F. '{print $NF}') ;
-      mv /home/sdavenport/freesurfer_runs/mris_preproc/tmp.mris_preproc.${pid} /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic${n}_fwhm${f}.dir
-done
+      rm -f /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic${n}.mgz ;
+      pid=$(awk '/tmpdir is/ {print $NF}' /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic${n}.mris_preproc.log | awk -F. '{print $NF}') ;
+      mv /home/sdavenport/freesurfer_runs/mris_preproc/tmp.mris_preproc.${pid} /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic${n}.dir
 done
 done
 done
