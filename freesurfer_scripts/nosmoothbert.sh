1528e502e165c7943f04ee1dfeed87d03bd064a7
BrainStatsSam <12SDavenport@gmail.com>
2024-02-22 18:23:24 +0100

    backup

--- freesurfer_scripts/nosmoothbert.sh
+++ freesurfer_scripts/nosmoothbert.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-for n in 3 5 ; do
+for n in 5 ; do
 TARGET=fsaverage${n}
 mkdir -p  $/home/sdavenport/freesurfer_runs/mris_preproc
 LIST=""
 for sub in $(cat /home/sdavenport/freesurfer_runs/listofsubjects.csv) ; do  #listofsubjects.csv needs to contain the subject folder names i.e. OAS...
 LIST="${LIST} --s ${sub}"
 done
+for f in 0 5 10 15; do
 for meas in area thickness; do
 for h in lh rh ; do
       ${FREESURFER_HOME}/bin/mris_preproc ${LIST} \
@@ -14,11 +15,13 @@ for h in lh rh ; do
       --target ${TARGET} \
       --meas ${meas} \
       --surfreg sphere.reg \
-      --out /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic${n}.mgz \
+      --out /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic${n}_fwhm${f}.mgz \
+      --fwhm ${f} \
       --nocleanup ;
-      rm -f /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic${n}.mgz ;
-      pid=$(awk '/tmpdir is/ {print $NF}' /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic${n}.mris_preproc.log | awk -F. '{print $NF}') ;
-      mv /home/sdavenport/freesurfer_runs/mris_preproc/tmp.mris_preproc.${pid} /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic${n}.dir
+      rm -f /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic${n}_fwhm${f}.mgz ;
+      pid=$(awk '/tmpdir is/ {print $NF}' /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic${n}_fwhm${f}.mris_preproc.log | awk -F. '{print $NF}') ;
+      mv /home/sdavenport/freesurfer_runs/mris_preproc/tmp.mris_preproc.${pid} /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic${n}_fwhm${f}.dir
+done
 done
 done
 done
--- freesurfer_scripts/open_processed_bert_files.m
+++ freesurfer_scripts/open_processed_bert_files.m
@@ -6,11 +6,23 @@ addpath('/home/sdavenport/2024_spin_test_validity/freesurfer_scripts/')
 bert_origrh_area = palm_miscread('/usr/pubsw/packages/freesurfer/RH4-x86_64-R530/subjects/bert/surf/rh.area');
 bert_ic3_area = palm_miscread('/home/sdavenport/freesurfer_runs/mris_preproc/rh.area.ic3_fwhm0.dir/bert.1.mgh');
 
+f = find(bert_origrh_area.data == bert_ic3_area.data(1));
+
+%%
+bert_origrh_thickness = palm_miscread('/usr/pubsw/packages/freesurfer/RH4-x86_64-R530/subjects/bert/surf/rh.thickness');
+bert_ic3_thickness = palm_miscread('/home/sdavenport/freesurfer_runs/mris_preproc/rh.thickness.ic3_fwhm0.dir/bert.1.mgh');
+
+f = find(bert_origrh_thickness.data == bert_ic3_thickness.data(1));
+
 %%
 addpath('C:\Users\12SDa\davenpor\Data\Surface\freesurfer_files\matlab')
 areadata = palm_miscread('C:/Users/12Sda/davenpor/Data/Oasis/oasis_cs_freesurfer_disc1/disc1/OAS1_0001_MR1/surf/rh.area');
 
 %%
 srf = fs2surf('C:/Users/12Sda/davenpor/Data/Oasis/oasis_cs_freesurfer_disc1/disc1/OAS1_0001_MR1/surf/rh.white');
+
+%%
+areadata = palm_miscread('C:/Users/12Sda/davenpor/Data/Oasis/oasis_cs_freesurfer_disc1/disc1/OAS1_0001_MR1/mri/brain.mgz');
+
 
 
