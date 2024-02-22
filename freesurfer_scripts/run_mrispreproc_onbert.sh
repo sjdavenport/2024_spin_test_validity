c214a7370403aa17c3a527b0da048b3520f906d4
BrainStatsSam <12SDavenport@gmail.com>
2024-02-22 09:43:47 +0100

    backup

--- freesurfer_scripts/run_mrispreproc_onbert.sh
+++ freesurfer_scripts/run_mrispreproc_onbert.sh
@@ -6,6 +6,7 @@ LIST=""
 for sub in $(cat /home/sdavenport/freesurfer_runs/listofsubjects.csv) ; do  #listofsubjects.csv needs to contain the subject folder names i.e. OAS...
 LIST="${LIST} --s ${sub}"
 done
+for f in 0 5 10 15; do
 for meas in area thickness; do
 for h in lh rh ; do
       ${FREESURFER_HOME}/bin/mris_preproc ${LIST} \
@@ -13,15 +14,17 @@ for h in lh rh ; do
       --target ${TARGET} \
       --meas ${meas} \
       --surfreg sphere.reg \
-      --out /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic3.mgz \
+      --out /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic3_fwhm${f}.mgz \
       --fwhm 15 \
       --nocleanup ;
-      rm -f /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic3.mgz ;
-      pid=$(awk '/tmpdir is/ {print $NF}' /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic3.mris_preproc.log | awk -F. '{print $NF}') ;
-      mv /home/sdavenport/freesurfer_runs/mris_preproc/tmp.mris_preproc.${pid} /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic3.dir
+      rm -f /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic3_fwhm${f}.mgz ;
+      pid=$(awk '/tmpdir is/ {print $NF}' /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic3_fwhm${f}.mris_preproc.log | awk -F. '{print $NF}') ;
+      mv /home/sdavenport/freesurfer_runs/mris_preproc/tmp.mris_preproc.${pid} /home/sdavenport/freesurfer_runs/mris_preproc/${h}.${meas}.ic3_fwhm${f}.dir
+done
 done
 done
 
 # interpolation is nearest neighbour
 # freesurfer contained here: /usr/pubsw/packages/freesurfer/RH4-x86_64-R530/
 # fsaverage/surf/ls.sphere is the icosahedron
+# /home/sdavenport/freesurfer_runs/mris_preproc/lh.thickness.ic3.dir
