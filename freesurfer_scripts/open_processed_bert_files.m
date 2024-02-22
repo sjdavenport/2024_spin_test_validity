d54e551401bdcd7ef3da8fd34fe62fb27249bc0c
BrainStatsSam <12SDavenport@gmail.com>
2024-02-22 12:05:37 +0100

    backup

--- freesurfer_scripts/open_processed_bert_files.m
+++ freesurfer_scripts/open_processed_bert_files.m
@@ -1,2 +1,5 @@
 % Opening matlab: /usr/pubsw/packages/matlab/R2023a/bin/glnxa64/MATLAB
 
+addpath('/usr/pubsw/packages/freesurfer/RH4-x86_64-R530/matlab')
+a = MRIread('/usr/pubsw/packages/freesurfer/RH4-x86_64-R530/subjects/bert/surf/rh.area');
+
